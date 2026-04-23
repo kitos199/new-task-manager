2d27ae8036b0dcf7dba340c9e72005091cd31ae4
Nikita <nik-ta@mail.ru>
2026-04-23 21:44:39 +0300

    output

---
+++ src/output.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector(".wrapper");
+
+const markup = ` <h1> task manager</h1>
+        <p>Войдите, чтобы управлять своими задачами</p>
+        <form action="" class="form-output">
+            <input class="login" type="text" name="name" placeholder="Ваше имя">
+            <input class="submit" type="submit" value="Войти">
+        </form>`;
+
+const init = (formTyupe) => {
+  const formOutput = document.querySelector(".form-output");
+  formOutput.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const getFormValue = new FormData(formOutput);
+    const fromData = Object.fromEntries(getFormValue);
+      console.log(fromData.name);
+     
+      if (fromData.name === '') {
+          alert("Ввидите имя")
+      }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  wrapper.insertAdjacentHTML("afterbegin", markup);
+  init();
+});
