735764922ace36ab8b7c763ddcbba1b92e339239
Nikita <nik-ta@mail.ru>
2026-05-18 13:22:26 +0300

    fixed

--- src/manager.js
+++ src/manager.js
@@ -72,21 +72,23 @@ const updateCart = () => {
       const redact = prompt("Редактирование")
       const taskCArt = e.target.closest(".task-cart");
       if (taskCart) {
-        document.querySelector(".task-text").textContent = `${redact}`
+      taskCArt.querySelector(".task-text").textContent = `${redact}`
      }
       // document.querySelector('.task-text').textContent=`${redact}`
     } else if (e.target.classList.contains("basket")) {
       taskCart.remove()
     } else if (e.target.classList.contains("checkbox-cart")) {
-      const anyChecked = document.querySelector(".checkbox-cart:checked");
-      const card = e.target.closest('.cart');
-      const textElement= card.querySelector('.task-text');
+      const anyChecked = taskCart.querySelector(".checkbox-cart:checked");
+      console.log(anyChecked, e.target);
+      // const card = e.target.closest('.cart');
+      // const textElement= card.querySelector('.task-text');
       if (anyChecked) {
-        textElement.classList.add("line-through");
-        console.log(textElement);
+        e.target.closest(".task-cart").querySelector(".task-text").classList.add("line-through");
+        // textElement.classList.add("line-through");
+        // console.log(textElement);
       } else {
         textElement.classList.remove("line-through");
-        console.log(textElement);
+        // console.log(textElement);
       }
     }
   })
